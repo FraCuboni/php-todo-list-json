005af49fd7253f888f616052e1d8c37a5b6e920b
FraCuboni <fracuboni@gmail.com>
2024-08-12 16:55:38 +0200

    feat creating base style

--- index.php
+++ index.php
@@ -5,10 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- LINK -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
 
+    <div class="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <div class="col-6">
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>a</th>
+                        <th>a</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>nome X</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-danger">Danger</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>nome X</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-danger">Danger</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>nome X</td>
+                        <td>
+                            <button type="button" class="btn btn-outline-danger">Danger</button>
+                        </td>
+                    </tr>
+                </tbody>
+        </div>
+        </table>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
